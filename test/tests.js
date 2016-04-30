eaa66ad83a0473a46d7d3147e301b582b7364fa3
Jonathan Brink <jonathandavidbrink@gmail.com>
2016-04-29 20:30:34 -0400

    use qunit module

--- test/tests.js
+++ test/tests.js
@@ -19,11 +19,17 @@ window.ticker_runTests = function() {
     var iSafeDelay = 500;
 
     jQuery('#runTestsButton').prop("disabled", true);
-    window._ticker.kill();
+
+    QUnit.module("test ticker-log", {
+        beforeEach: function() {
+            ticker.kill();
+        },
+        afterEach: function() {
+            ticker.reset();
+        }
+    });
 
     QUnit.test("test config", function(assert) {
-        window._ticker.kill();
-
         window._ticker.config({
             'foo': 'bar',
             'interval': 999
@@ -33,8 +39,6 @@ window.ticker_runTests = function() {
     });
 
     QUnit.test("does log appear", function(assert) {
-        window._ticker.kill();
-
         // at the start there are no log divs
         assert.strictEqual(howManyLogDivs(), 0, "zero log divs are present");
 
@@ -52,8 +56,6 @@ window.ticker_runTests = function() {
     });
 
     QUnit.test("print api", function(assert) {
-        window._ticker.kill();
-
         ticker.print('lorum ipsum');
         assert.strictEqual(howManyLogDivs(), 1, "print api works");
         assert.strictEqual('lorum ipsum', getText(0), 'correct text');
@@ -61,8 +63,6 @@ window.ticker_runTests = function() {
 
     /* TODO: need to reset "output" settings
     QUnit.test("print api with output textarea", function(assert) {
-        window._ticker.kill();
-
         ticker.print('lorum ipsum', {
             textarea: true
         });
@@ -74,8 +74,6 @@ window.ticker_runTests = function() {
     */
 
     QUnit.test("namespace", function(assert) {
-        window._ticker.kill();
-
         // at the start there are no log divs
         assert.strictEqual(howManyLogDivs(), 0, "zero log divs are present");
 
@@ -85,8 +83,6 @@ window.ticker_runTests = function() {
     });
 
     QUnit.test("output", function (assert) {
-        window._ticker.kill();
-
         // at the start there is no output textarea
         assert.strictEqual(getTextarea().length, 0, "no output textarea present");
 
@@ -106,8 +102,6 @@ window.ticker_runTests = function() {
     });
 
     QUnit.test("dump", function(assert) {
-        window._ticker.kill();
-
         // make sure no logging showing
         assert.strictEqual(howManyLogDivs(), 0, "no log divs at start");
 
@@ -119,8 +113,6 @@ window.ticker_runTests = function() {
     });
 
     QUnit.test("help", function(assert) {
-        window._ticker.kill();
-
         // make sure no logging showing
         assert.strictEqual(howManyLogDivs(), 0, "no log divs at start");
 
@@ -132,7 +124,6 @@ window.ticker_runTests = function() {
     });
 
     QUnit.test("pause", function(assert) {
-        window._ticker.kill();
         jQuery('#pauseButton').trigger("click");
         assert.strictEqual(window._ticker._oConfig.pauseMode, true, 'test pauseMode property');
     });
@@ -144,8 +135,6 @@ window.ticker_runTests = function() {
         function isRight() {
             assert.ok(jQuery('._ticker_log').offset().left > 0, "logs are to the right of the screen");
         }
-
-        window._ticker.kill();
 
         // by default should be left
         window._ticker.test();
@@ -165,7 +154,6 @@ window.ticker_runTests = function() {
     });
 
     QUnit.test("backtick and requireBackTick", function(assert) {
-        window._ticker.kill();
         console.log('`', 'lorum ipsum');
         assert.strictEqual(howManyLogDivs(), 1, "(back-tick used) one div present because back-tick used in console statement");
 
@@ -187,7 +175,6 @@ window.ticker_runTests = function() {
     });
 
     QUnit.test("macro", function(assert) {
-        ticker.kill();
         ticker.registerMacro(8, function() {
             console.log('`', 'testing macro 8');
         });
@@ -198,7 +185,6 @@ window.ticker_runTests = function() {
     });
 
     QUnit.test("macro 9 - don't allow registerMacro api", function(assert) {
-        ticker.kill();
         ticker.registerMacro(9, function() {
             // this should never happen
             assert.ok(false, "should never execute macro 9 when using registerMacro api");
@@ -208,7 +194,6 @@ window.ticker_runTests = function() {
     });
 
     QUnit.test("macro 9 (macroEdit)", function(assert) {
-        ticker.kill();
         ticker.macroEdit();
         getTextarea().text("console.log('`', 'lorum ipsum');");
         ticker.macroEdit();
@@ -223,8 +208,6 @@ window.ticker_runTests = function() {
     });
 
     QUnit.test("channels", function(assert) {
-        ticker.kill();
-
         assert.strictEqual('log', window._ticker._oConfig.channel, 'default channel is log');
         window._ticker.nextChannel();
         assert.strictEqual('debug', window._ticker._oConfig.channel, 'after change channel we are at debug');
@@ -241,7 +224,6 @@ window.ticker_runTests = function() {
     // keep this as the final test
     QUnit.test("final", function(assert) {
         var done = assert.async();
-        window._ticker.kill();
         assert.ok(true, "");
         setTimeout(function() {
             jQuery('#runTestsButton').prop("disabled", false);
