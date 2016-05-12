654b95edcb683120ed94306a143538dad93face8
Jonathan Brink <jonathandavidbrink@gmail.com>
2016-05-11 20:50:29 -0400

    show timestamp above built code

--- bin/package.sh
+++ bin/package.sh
@@ -2,7 +2,8 @@
 
 mkdir -p dist
 jsdoc --private src/ticker.js
-uglifyjs src/ticker.js --compress --mangle > dist/ticker.min.js
+echo "/*" `date` "*/" > dist/ticker.min.js
+uglifyjs src/ticker.js --compress --mangle >> dist/ticker.min.js
 cp src/ticker.js dist/ticker.js
 mkdir -p dist/jsdoc && cp -R out/* dist/jsdoc
 
