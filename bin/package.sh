c6495605fc9b255a92bb9415fe02e9dfde1cc031
Jonathan Brink <jonathandavidbrink@gmail.com>
2016-12-14 18:49:03 -0500

    simplified folder structure
    Fixes #35

--- bin/package.sh
+++ bin/package.sh
@@ -5,7 +5,7 @@ version=`grep version ./package.json | \
     awk -F "\"" '{ print $4 }'`
 
 echo "/* https://github.com/jonbri/ticker-log v$version" `date` "*/" > dist/ticker-log.min.js
-uglifyjs src/ticker.js --compress --mangle >> dist/ticker-log.min.js
-cp src/ticker.js dist/ticker.js
-jsdoc -d "./dist/jsdoc" --private src/ticker.js
+uglifyjs ticker.js --compress --mangle >> dist/ticker-log.min.js
+cp ticker.js dist/ticker.js
+jsdoc -d "./dist/jsdoc" --private ticker.js
 
