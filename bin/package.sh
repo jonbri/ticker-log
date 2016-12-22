1f4cc00a9bb845d4222e4f9f4415f2da0a68e91b
Jonathan Brink <jonathandavidbrink@gmail.com>
2016-12-22 13:33:07 -0500

    ticker.js -> ticker-log.js

--- bin/package.sh
+++ bin/package.sh
@@ -5,7 +5,7 @@ version=`grep version ./package.json | \
     awk -F "\"" '{ print $4 }'`
 
 echo "/* https://github.com/jonbri/ticker-log v$version" `date` "*/" > dist/ticker-log.min.js
-uglifyjs ticker.js --compress --mangle >> dist/ticker-log.min.js
-cp ticker.js dist/ticker.js
-jsdoc -d "./dist/jsdoc" --private ticker.js
+uglifyjs ticker-log.js --compress --mangle >> dist/ticker-log.min.js
+cp ticker-log.js dist/ticker-log.js
+jsdoc -d "./dist/jsdoc" --private ticker-log.js
 
