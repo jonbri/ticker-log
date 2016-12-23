d8a2b498d3f50362e246dfb612548ab45b1fcb31
Jonathan Brink <jonathandavidbrink@gmail.com>
2016-12-23 08:34:36 -0500

    generate.sh uses correct dist file

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 
 # make staging area
 # clone ticker-log so I can get:
-# * dist/ticker.min.js
+# * dist/ticker-log.min.js
 # * dist/jsdoc
 rm -Rf tmp
 mkdir tmp
@@ -18,7 +18,7 @@ perl -i -nl -e '
 
   # if ticker-lib, replace with new version
   elsif (/function ticker_go/) {
-    open FH, "<", "./tmp/ticker-log/dist/ticker.min.js";
+    open FH, "<", "./tmp/ticker-log/dist/ticker-log.min.js";
     my $lib = do { local $/; <FH> };
     close FH;
     chomp $lib;
@@ -37,5 +37,7 @@ cp -R ./tmp/ticker-log/dist/jsdoc .
 # print out the current version
 grep version ./tmp/ticker-log/package.json | \
     awk -F "\"" '{ print "Current version:",$4 }'
+
+echo
 
 rm -Rf tmp
