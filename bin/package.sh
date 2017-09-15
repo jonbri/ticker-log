69cbc59bf1705d4e44e2c8b1047542874ada3c0f
Jonathan Brink <jonathandavidbrink@gmail.com>
2017-09-14 21:26:08 -0400

    ticker-log.js -> index.js
    Fixes #46

--- bin/package.sh
+++ bin/package.sh
@@ -5,7 +5,7 @@ version=`grep version ./package.json | \
     awk -F "\"" '{ print $4 }'`
 
 echo "/* https://github.com/jonbri/ticker-log v$version" `date` "*/" > dist/ticker-log.min.js
-uglifyjs ticker-log.js --compress --mangle >> dist/ticker-log.min.js
-cp ticker-log.js dist/ticker-log.js
-jsdoc -d "./dist/jsdoc" --private ticker-log.js
+uglifyjs index.js --compress --mangle >> dist/ticker-log.min.js
+cp index.js dist/index.js
+jsdoc -d "./dist/jsdoc" --private index.js
 
